7aa4742710d5d6cb5db3350b2a6b18c7c8590d12
codyxrat <mathias.sorhaug@hotmail.com>
2024-10-21 23:05:18 +0200

    Mal
    basicasbasiccanbe

---
+++ Mal.java
@@ -0,0 +1,19 @@
+import static java.lang.System.*;
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.*;
+import static java.lang.Math.*;
+
+public class Mal {
+
+  public static void main(String[] args) {
+    // Skriv Java-setninger her
+    // leser inn lengden fra brukeren
+    String talltekst = showInputDialog("Gi tall tommer:");
+    int antallTommer = parseInt(talltekst);
+
+    String utTekst = "";
+    showMessageDialog(null, utTekst);
+
+  }
+
+}
